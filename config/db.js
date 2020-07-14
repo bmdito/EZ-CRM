f546c0253191bcfe12fe1e9daccc9461929c7ec8
bdiddy <44068902+bmdito@users.noreply.github.com>
2020-07-13 23:55:32 -0700

    added mongodb with config

---
+++ config/db.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const config = require("config");
+//allows us to get any values from default json
+const db = config.get("mongoURI");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+
+    console.log("MongoDB Connected...");
+  } catch (err) {
+    console.error(err.message);
+    //exit process with failure
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
