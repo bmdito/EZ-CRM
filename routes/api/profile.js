44157e65d369bef2f848b4fe9f7f115a978df821
bdiddy <44068902+bmdito@users.noreply.github.com>
2020-07-15 01:29:14 -0700

    added POST for create and update profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const auth = require("../../middleware/auth");
 const Profile = require("../../models/Profile");
 const User = require("../../models/User");
+const { check, validationResult } = require("express-validator");
 
 // @route   GET api/profile/me
 // @desc    Get current users profile
@@ -23,6 +24,54 @@ router.get("/me", auth, async (req, res) => {
     res.status(500).send("Server error");
   }
 });
+
+// @route   POST api/profile
+// @desc    Create or update user profile
+// @access  Private
+
+//need brackets below because we are using auth middleware AND express validator
+router.post(
+  "/",
+  [auth, [check("name", "name is required").not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { company, location, bio, mainUser } = req.body;
+
+    //build profile object
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if (company) profileFields.company = company;
+    if (location) profileFields.location = location;
+    if (bio) profileFields.bio = bio;
+    if (mainUser) profileFields.mainUser = mainUser;
+
+    try {
+      let profile = await Profile.findOne({ user: req.user.id });
+      if (profile) {
+        profile = await Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        );
+
+        return res.json(profile);
+      }
+
+      //create profile if none existed
+      profile = new Profile(profileFields);
+
+      await profile.save();
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
+  }
+);
 
 //export router
 module.exports = router;
